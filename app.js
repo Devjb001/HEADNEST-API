6201009ebe58b64676d881efde5fcb277cd2875d
Devjb001 <suleelijah13@gmail.com>
2025-10-07 15:23:03 +0100

    uncomment agenda

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 require("dotenv").config()
-// const agenda = require("./src/config/agenda");
+const agenda = require("./src/config/agenda");
 require("./src/jobs/sendReminders");
 const app = express();
 
