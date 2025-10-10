eb1d89688fef8ba165582ffa9e1fafa2d063bd77
Tega Rorobi <rorobitega@gmail.com>
2025-10-10 22:34:10 +0100

    feat: Add logging middleware for requests

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 
 //connecting to database
 connectToDataBase()
